3c0521442aa42a23fe34c94f11c525ae68974bc0
Courvix <Courvix@users.noreply.github.com>
2021-01-07 10:48:10 +0000

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,40 +1,13 @@
-/* 
-    About:
-
-    Made by Courvix for the Courvix Network
-    Twitter: 0x64c
-    Instagram: courvixnetwork
-    Email: contact@courvix.com
-    GitHub: Courvix
-    GitLab: Courvix
-
-    Requirements:
-
-    phin - npm i install phin - used for as a http lib to send request to discord webhook
-    chalk - npm i install chalk - used for easier text colors lol
-    ovh - npm i install ovh - ovh's own lib for their api functions, includes auto json parsing
-
-    How to Use:
-
-    1.) Install the requirements mentioned above
-    2.) Generate OVH API tokens with their pages (
-        https://eu.api.ovh.com/createToken/ - for EU region
-        https://ca.api.ovh.com/createToken/ - for CA region
-        https://api.ovh.com/createToken/ - for US region
-    )
-    3.) Use the keys generated and configure them below where it says endpoint, appKey, appSecret, consumerKey
-    4.) Customize anything else
-    5.) Run the checker
-*/
+// Made by Courvix for the Courvix Network
 
 const phin = require('phin')
 const chalk = require('chalk')
 const net = require('net')
 const ovh = require('ovh')({
     endpoint: "ovh-eu", // Endpoint here - it is based on which OVH region your account is with
-    appKey: "APP KEY HERE",
-    appSecret: "APP SECRET KEY HERE",
-    consumerKey: "CONSUMER KEY HERE"
+    appKey: "",
+    appSecret: "",
+    consumerKey: ""
 });
 
 const discordURL = "WEBHOOK HERE";
@@ -43,35 +16,41 @@ const host = "HOST/ISP";
 const protectionProvider = "PROTECTION PROVIDER";
 const ipBlock = process.argv[2];
 const ipAddr = process.argv[3];
+const interval = process.argv[4] * 1000;
 let mitigationEnabled = false;
 
-if (process.argv.length < 4 || !net.isIPv4(ipAddr)) {
-    console.log(chalk.magenta("Usage: node checker [IP Block] [IP Address]"));
+if (process.argv.length < 5 || !net.isIPv4(ipAddr)) {
+    console.log(chalk.magenta("Usage: node checker [IP Block] [IP Address] [Check Interval]"));
 } else {
     CheckMitigation();
 }
 
 async function CheckMitigation() {
+    // noinspection InfiniteLoopJS
     while (true) {
         let time = new Date();
-        console.log(chalk.white.bold(`Checking mitigation status ${time.toLocaleTimeString()}`));
+        console.log(chalk.white.bold(`[info] : Checking mitigation status ${time.toLocaleTimeString()}`));
         ovh.request("GET", `/ip/${encodeURIComponent(ipBlock)}/mitigation/${ipAddr}`, (err, mitigationStatus) => {
-            console.log(chalk.yellow(`IP Address: ${ipAddr} | Auto Mode: ${mitigationStatus.auto} | Permanent Mode: ${mitigationStatus.permanent}`));
-            if (mitigationStatus.permanent === true && mitigationStatus.auto === true) {
-                if (mitigationEnabled === true) {
-                    console.log("Mitigation remains enabled.")
-                } else if (mitigationEnabled === false) {
-                    console.log("Mitigation mode enabled, sending alert");
-                    mitigationEnabled = true;
-                    SendAlert(true);
+            if(!err) {
+                console.log(chalk.green(`IP Address: ${ipAddr} | Auto Mode: ${mitigationStatus.auto} | Permanent Mode: ${mitigationStatus.permanent}`));
+                if (mitigationStatus.permanent === true && mitigationStatus.auto === true) {
+                    if (mitigationEnabled === true) {
+                        console.log(chalk.blackBright.red("[warn] : Mitigation mode is still enabled"));
+                    } else if (mitigationEnabled === false) {
+                        console.log(chalk.blackBright.red("[warn] : Mitigation mode has been enabled"));
+                        mitigationEnabled = true;
+                        SendAlert(true);
+                    }
+                } else if (mitigationStatus.auto === false && mitigationEnabled === true) {
+                    console.log(chalk.green("[info] : Mitigation mode has been disabled"));
+                    mitigationEnabled = false;
+                    SendAlert(false);
                 }
-            } else if (mitigationStatus.auto === false && mitigationEnabled === true) {
-                console.log("Mitigation mode disabled, sending alert");
-                mitigationEnabled = false;
-                SendAlert(false);
+            } else {
+                console.log(chalk.blackBright.red(`[crit] : ${err} response (${mitigationStatus})`));
             }
         });
-        await new Promise(r => setTimeout(r, 5000));
+        await new Promise(r => setTimeout(r, interval));
     }
 }
 
@@ -136,5 +115,5 @@ async function SendAlert(mode) {
     })
 
     const res = await request(discordURL);
-    console.log(res);
+    return(res);
 }
